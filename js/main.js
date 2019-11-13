7c7f5586ccb2df3fd974a6bf792c3ea603766e48
707397376@qq.com <gbljw123456>
2019-11-13 22:55:24 +0800

    add

--- js/main.js
+++ js/main.js
@@ -11,12 +11,27 @@ var Drainage = {
                 showLabel: false,
                 expandZoomRange: true,
             });
-            
+
             dmodule.find('.J_screen').click(function () {
-                layer.open({
-                    className: 'screen-popup',
-                    content: '内容'
-                })
+                dmodule.find('.J_msgPopup').show()
+                dmodule.find('.J_maskBox').show()
+            })
+            dmodule.find('.J_maskBox').click(function () {
+                $(this).hide()
+                dmodule.find('.J_msgPopup').hide()
+            })
+        },
+        //办公-人员-统计
+        screenRyTj: function (id) {
+            var dmodule = $('#' + id);
+            dmodule.find('.J_screen').click(function () {
+
+                dmodule.find('.J_msgPopup').show()
+                dmodule.find('.J_maskBox').show()
+            })
+            dmodule.find('.J_maskBox').click(function () {
+                $(this).hide()
+                dmodule.find('.J_msgPopup').hide()
             })
         },
         //运动轨迹详情
@@ -43,9 +58,13 @@ var Drainage = {
 
             dmodule.find('.J_navList').on('click', 'li', function () {
                 $(this).addClass('active').find('img').attr('src', $(this).find('img').attr('data-active')).end().siblings().removeClass('active')
-                $(this).siblings().each(function(index,el){
-                   $(el).find('img').attr('src',$(el).find('img').attr('data-src'))                     
+                $(this).siblings().each(function (index, el) {
+                    $(el).find('img').attr('src', $(el).find('img').attr('data-src'))
                 })
+            })
+
+            dmodule.find('.J_closeBtn').click(function () {
+                dmodule.find('.J_tipsMsg').hide()
             })
         },
         //一日一查首页
@@ -59,22 +78,37 @@ var Drainage = {
                 expandZoomRange: true,
             });
         },
-        //实时雨晴
-        realTime:function(id){
+        //一日一查-选择河道
+        riverList: function (id) {
             var dmodule = $('#' + id);
-            dmodule.find('.J_tabClick').click(function () { 
-			console.log( $(this).children('p'))
-                    $(this).children('p').addClass("active");
-                    $(this).siblings().children('p').removeClass("active");
-                    $(this).addClass("active").siblings().removeClass("active");
-                     dmodule.find(".products .mainCont").eq($(this).index()).show().siblings().hide();
-                
-               
-                
+            dmodule.find('.J_listBox').on('click', 'li', function () {
+                layer.open({
+                    content: `您确认要选择${$(this).find('a').text()}`
+                    , btn: ['确认', '取消']
+                    , yes: function (index) {
+                        location.href = $(this).attr('data-url')
+                        layer.close(index);
+                    }
+                });
+            })
+        },
+
+        //实时雨晴
+        realTime: function (id) {
+            var dmodule = $('#' + id);
+            dmodule.find('.J_tabClick').click(function () {
+                console.log($(this).children('p'))
+                $(this).children('p').addClass("active");
+                $(this).siblings().children('p').removeClass("active");
+                $(this).addClass("active").siblings().removeClass("active");
+                dmodule.find(".products .mainCont").eq($(this).index()).show().siblings().hide();
+
+
+
             })
 
         }
-           
+
 
     }
 }
@@ -90,6 +124,8 @@ $(function () {
     doWhileExist('officeWeather', Drainage.Page.officeWeather);
     doWhileExist('checkIndex', Drainage.Page.checkIndex);
     doWhileExist('realTime', Drainage.Page.realTime);
+    doWhileExist('screenRyTj', Drainage.Page.screenRyTj);
+    doWhileExist('riverList', Drainage.Page.riverList);
 })
 
 
