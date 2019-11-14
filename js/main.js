7f4967e3505c0a78692011d385ab8372a56fe4dd
maomengzhen <maomengzhen@pipikou.com>
2019-11-14 09:11:42 +0800

    基础信息

--- js/main.js
+++ js/main.js
@@ -59,23 +59,52 @@ var Drainage = {
         //实时雨晴
         realTime:function(id){
             var dmodule = $('#' + id);
-            dmodule.find('.J_tabClick').click(function () { 
+            dmodule.find('.J_tabClick').on('click','li',function(){ 
 			console.log( $(this).children('p'))
                     $(this).children('p').addClass("active");
                     $(this).siblings().children('p').removeClass("active");
                     $(this).addClass("active").siblings().removeClass("active");
-                     dmodule.find(".products .mainCont").eq($(this).index()).show().siblings().hide();
+                    dmodule.find(".products .main-cont").eq($(this).index()).show().siblings().hide();
                 
                
                 
             })
 
-        }
-           
+        },
+        //闸泵水情
+        gatePump:function(id){
+            var dmodule = $('#' + id);
+            dmodule.find('.J_tabClick').on('click','li',function(){ 
+                $(this).children('p').addClass("active");
+                $(this).siblings().children('p').removeClass("active");
+                $(this).addClass("active").siblings().removeClass("active");
+                dmodule.find(".products .main-cont").eq($(this).index()).show().siblings().hide();
+                
+               
+                
+            })
+
+        },
+        // 实时雨晴--基本信息
+        basicDetails:function(id){
+            var dmodule = $('#' + id);
+            dmodule.find('.J_tabClick').on('click','li',function(){ 
+                $(this).children('p').addClass("active");
+                $(this).siblings().children('p').removeClass("active");
+                $(this).addClass("active").siblings().removeClass("active");
+                dmodule.find(".products .main-cont").eq($(this).index()).show().siblings().hide();
+        })
+        dmodule.find('.J_screen').click(function () {
+            layer.open({
+                className: 'screen-popup',
+                content: '内容'
+            })
+        })
+       
 
     }
 }
-
+}
 $(function () {
     var doWhileExist = function (sModuleId, oFunction) {
         if (document.getElementById(sModuleId)) {
@@ -87,6 +116,8 @@ $(function () {
     doWhileExist('officeWeather', Drainage.Page.officeWeather);
     doWhileExist('checkIndex', Drainage.Page.checkIndex);
     doWhileExist('realTime', Drainage.Page.realTime);
+    doWhileExist('gatePump', Drainage.Page.gatePump);
+    doWhileExist('basicDetails', Drainage.Page.basicDetails);
 })
 
 
